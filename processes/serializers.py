f1a8b235788cf6c5711623b46a6cbbf081c0d530
Anthony Rivas <arivasc@unsa.edu.pe>
2024-12-09 11:05:47 -0500

    fix processes create validation

--- processes/serializers.py
+++ processes/serializers.py
@@ -1,12 +1,39 @@
 from rest_framework import serializers
 from processes.models import Process
+import json
 
 class DetailedProcessSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Process
-        fields = ['id', 'name', 'description', 'yearOfEntry', 'yearProcess', 'dateStart', 'dateEnd', 'importantDates', 'shifts', 'processType', 'registerState']
+  class Meta:
+    model = Process
+    fields = [
+      'id',
+      'name',
+      'description',
+      'yearOfEntry',
+      'yearProcess',
+      'dateStart',
+      'dateEnd',
+      'importantDates',
+      'shifts',
+      'processType',
+      'registerState'
+      ]
+  
+  # Convertir JSON a string antes de guardar
+  def to_internal_value(self, data):
+    data['importantDates'] = json.dumps(data.get('importantDates', {}))
+    data['shifts'] = json.dumps(data.get('shifts', {}))
+    return super().to_internal_value(data)
+  
+  # Convertir string a JSON al devolver los datos
+  def to_representation(self, instance):
+    representation = super().to_representation(instance)
+    representation['importantDates'] = json.loads(instance.importantDates)
+    representation['shifts'] = json.loads(instance.shifts)
+    return representation
+        
         
 class SimpleListProcessSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Process
-        fields = ['id', 'name', 'yearProcess', 'dateStart', 'dateEnd', 'registerState']
+  class Meta:
+    model = Process
+    fields = ['id', 'name', 'yearProcess', 'dateStart', 'dateEnd', 'registerState']
