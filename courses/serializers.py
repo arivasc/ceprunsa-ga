280c289e8bfcbfbb1c260f46999c95de19de3683
Anthony Rivas <arivasc@unsa.edu.pe>
2024-12-22 20:40:01 -0500

    list view courses personal

--- courses/serializers.py
+++ courses/serializers.py
@@ -94,36 +94,17 @@ class CourseTeacherRelationSerializer(serializers.ModelSerializer):
 #================================================================
 class DetailedCourseTeacherRelationSerializer(serializers.ModelSerializer):
   
-  courseId = serializers.IntegerField(source='course.id', read_only=True)
-  courseName = serializers.CharField(source='course.name', read_only=True)
-  
   teacherId = serializers.IntegerField(source='teacher.id', read_only=True)
-  teacherNames = serializers.SerializerMethodField()
+  teacherNames = serializers.CharField(source='teacher.userceprunsapersonalinfo.names', read_only=True)
+  teacherLastName = serializers.CharField(source='teacher.userceprunsapersonalinfo.lastNames', read_only=True)
+  teacherDocument = serializers.CharField(source='teacher.userceprunsapersonalinfo.document', read_only=True)
   
   class Meta:
     model = CourseTeacherRelation
     fields = [
       'id',
-      'courseId',
-      'courseName',
       'teacherId',
-      'teacherNames'
+      'teacherNames',
+      'teacherLastName',
+      'teacherDocument'
     ]
-    
-  def get_teacherNames(self, obj):
-    teacher = obj.teacher
-    if teacher and hasattr(teacher, 'userceprunsapersonalinfo'):
-      personal_info = teacher.userceprunsapersonalinfo
-      first_name = personal_info.names.split(' ')[0]
-      last_names = personal_info.lastNames.split(' ')
-     
-      second_name_initial = (
-          personal_info.names.split(' ')[1][0] + '.'
-          if len(personal_info.names.split(' ')) > 1 else ''
-      )
-      last_name_1 = last_names[0] if len(last_names) > 0 else ''
-      last_name_2_initial = (last_names[1][0] + '.') if len(last_names) > 1 else ''
-      
-      formatted_name = f"{first_name} {second_name_initial} {last_name_1} {last_name_2_initial}"
-      return formatted_name
-    return None
